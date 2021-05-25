7cb07e2a97868d048196227f52f0d36c9999653b
Vurak <vurak.of.sulcan@gmail.com>
2021-05-25 20:06:48 +0100

    Installed Babel locally and moved JSX to its own file (App.js). Added node_modules to .gitignore. Only expected problem with this is rellax.js not working.

---
+++ src/text.js
@@ -0,0 +1,16 @@
+const projects = {
+    game: {
+        short: "",
+        long: ""
+    },
+    lmgtfy: {
+        short: "",
+        long: ""
+    },
+    sigmo: {
+        short: "",
+        long: ""
+    }
+}
+
+export default projects
